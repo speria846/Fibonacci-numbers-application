cd17859fc854097f19d8f438177eba23e8387842
speria846 <niwenyesigasperia@gmail.com>
2022-07-02 08:19:57 +0300

    first commit

--- app/src/main/java/com/speria/fibonacci_series_activity/MainActivity.kt
+++ app/src/main/java/com/speria/fibonacci_series_activity/MainActivity.kt
@@ -7,18 +7,32 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.speria.fibonacci_series_activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-   lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        displayNames()
+        var numberList=Addition(100)
+        print(numberList)
+        binding.rvNames.layoutManager=LinearLayoutManager(this)
+        binding.rvNames.adapter=NumberRecyclerViewAdapter(numberList)
 
     }
-    fun displayNames(){
-        val nameList= listOf(1,2,3,5,8,11,19,30,49,79,128)
-        binding.rvNames.layoutManager= LinearLayoutManager(this)
-        val namesAdapter=NumberRecyclerViewAdapter(nameList)
-        binding.rvNames.adapter=namesAdapter
+
+    fun Addition(size: Int): List<Int> {
+        var list = ArrayList<Int>()
+        var n1 = 1
+        var n2 = 0
+        var a = 0
+        while (a <= size) {
+            print(n1)
+            var sum =n1+n2
+            n1=n2
+            n2=sum
+            a++
+            list+=sum
+        }
+        return list
+
     }
 }
